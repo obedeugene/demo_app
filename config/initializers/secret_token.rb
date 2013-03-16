adf85ea544d8d6e5eb64b3ad9a25aab46f41e505
Obed Eugene <obed.eugene@gmail.com>
2013-03-15 22:10:58 -0400

    Created a Users resiyrce

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = 'e2b97f4c098b335aa6a50073d31e9338e0da8f005d5d9a8bf01ba6dba0b6823b4dd498832e78979cd1ce3ad1a01a6f75dbd9975f48ea793fe62220f83c2585ed'
