6330c7400111d16f577e179223ff03f70e0744b5
Obed Eugene <obed.eugene@gmail.com>
2013-03-16 08:06:48 -0400

    SImplified the User Controllers

--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,6 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130316020741) do
-
-  create_table "users", :force => true do |t|
-    t.string   "name"
-    t.string   "string"
-    t.string   "email"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
-  end
+ActiveRecord::Schema.define(:version => 0) do
 
 end
